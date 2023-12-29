46ab2fec7c21ba2ef4977db65682c60154184905
phsu40 <phsu40@gatech.edu>
2023-12-29 13:01:28 -0500

    Connect to PostgresSQL database

---
+++ clue-helper-server/db.js
@@ -0,0 +1,14 @@
+const pgp = require('pg-promise')();
+
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+}
+
+// Create  a new database instance
+const db = pgp(dbConfig);
+
+module.exports = db;
